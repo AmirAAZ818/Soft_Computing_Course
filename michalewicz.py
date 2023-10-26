0c8dcb31b6a6c1eb50e271fe4b1fc64e787eed13
Sadra Koocheckzadeh <141414480+Sadrakch@users.noreply.github.com>
2023-10-26 15:43:55 +0330

    michalewicz.py

---
+++ michalewicz.py
@@ -0,0 +1,9 @@
+import sin,pi from math
+
+def michalewicz(x):
+    m = 10
+    n = len(x)
+    result = 0
+    for i in range(n):
+        result -= sin(x[i]) * sin((i + 1) * x[i]**2 / pi)**(2 * m)
+    return result
