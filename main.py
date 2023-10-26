da81d103348a7f022ff56982953621eea0fc140e
hamid <h.r.bz1381@gmail.com>
2023-10-26 09:37:50 +0330

    add init funct

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 
 
 class BGA:
-    pass
+    def __init__(self, optimization_function, parameters_number, population, 
+                 crossover_rate, mutation_rate):
+        self.Function = optimization_function # تابع هدف
+        self.Parameters = parameters_number # تعداد پارامترهای تابع 
+        self.Population = population # اندازه جمعیت
+        self.Pc = crossover_rate
+        self.pm = mutation_rate
+        
