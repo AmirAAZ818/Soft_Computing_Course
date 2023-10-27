94d3b6d0464772d40cfb808992056b0678af1300
hamid <h.r.bz1381@gmail.com>
2023-10-27 10:44:04 +0330

    impliment mutation

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -1,12 +1,35 @@
+from random import randint, random
+
 
 # class for BGA Algo
 
 class BGA:
-    def __init__(self, optimization_function, parameters_number, population, 
+    def __init__(self, optimization_function, parameters_number,choromosom_bits, population, 
                  crossover_rate, mutation_rate):
         self.Function = optimization_function # تابع هدف
         self.Parameters = parameters_number # تعداد پارامترهای تابع 
         self.Population = population # اندازه جمعیت
+        self.Choromosom_Len = choromosom_bits
         self.Pc = crossover_rate
         self.pm = mutation_rate
-        
+
+
+    def Random_Choromosom(self):
+        population_matrix = []
+        for i in range(self.Population):
+            choro = []
+            for j in range(self.Choromosom_Len):
+                choro[j] = randint(0,1)
+            population_matrix[i] = choro
+        return population_matrix
+    
+
+    def Mutation(self, child_matrix):
+        rand = random()
+        if rand > self.pm :
+            return
+        else:
+            i = randint(0,self.Population-1)
+            j = randint(0,self.Choromosom_Len-1)
+            child_matrix[i,j] = not child_matrix[i,j]
+            return
