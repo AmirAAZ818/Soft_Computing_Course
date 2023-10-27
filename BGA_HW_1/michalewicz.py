8ad8947bbf71c39bc9ad37f67b8a0aec53173410
Sadra Koocheckzadeh <141414480+Sadrakch@users.noreply.github.com>
2023-10-27 11:03:48 +0330

    Delete BGA_HW_1/michalewicz.py
    To merge with a main problem.py file

--- BGA_HW_1/michalewicz.py
+++
@@ -1,9 +0,0 @@
-from math import sin, pi
-
-def michalewicz(x):
-    m = 10
-    n = len(x)
-    result = 0
-    for i in range(n):
-        result -= sin(x[i]) * sin((i + 1) * x[i]**2 / pi)**(2 * m)
-    return result
