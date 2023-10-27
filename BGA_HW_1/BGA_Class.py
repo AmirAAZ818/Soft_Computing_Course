fc9268770135ce7dbd9bc1a41a664c92d659185a
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-10-27 18:50:26 +0330

    updated BGA Class
    I changed a little bit of random chromosome method and also some of the names of the arguments
    also tested it to see if it was working, and it was :)

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -4,36 +4,51 @@ from random import randint, random
 # class for BGA Algo
 
 class BGA:
-    def __init__(self, optimization_function, parameters_number,choromosom_bits, population, 
-                 crossover_rate, mutation_rate):
-        self.Function = optimization_function # تابع هدف
-        self.Parameters = parameters_number # تعداد پارامترهای تابع 
-        self.Population = population # اندازه جمعیت
-        self.Choromosom_Len = choromosom_bits
-        self.Pc = crossover_rate
+    def __init__(self, target_function, function_dim, population,
+                 crossover_rate, mutation_rate, chromosome_bits=8):
+
+        self.population_matrix = None
+        self.function = target_function  # تابع هدف
+        self.parameters = function_dim  # تعداد پارامترهای تابع
+        self.population = population  # اندازه جمعیت
+        self.chromosome_len = chromosome_bits
+        self.pc = crossover_rate
         self.pm = mutation_rate
 
+    # For making random population
+    def Random_Chromosome(self):
+        population_matrix = []  # N x Sigma(Li) from i = 0 to function_dim
 
-    def Random_Choromosom(self):
-        population_matrix = []
-        for i in range(self.Population):
-            choro = []
-            for j in range(self.Choromosom_Len):
-                choro[j] = randint(0,1)
-            population_matrix[i] = choro
-        return population_matrix
+        for i in range(self.population):
+            chromosome = []
+            for j in range(self.chromosome_len):
+                chromosome.append(randint(0, 1))
+            population_matrix.append(chromosome)
+
+        self.population_matrix = population_matrix
+
     
-
     def Mutation(self, child_matrix):
         rand = random()
-        if rand > self.pm :
+        if rand > self.pm:
             return
         else:
-            i = randint(0,self.Population-1)
-            j = randint(0,self.Choromosom_Len-1)
-            child_matrix[i,j] = not child_matrix[i,j]
+            i = randint(0, self.population - 1)
+            j = randint(0, self.chromosome_len - 1)
+            child_matrix[i, j] = not child_matrix[i, j]
             return
-        
-    
-    def Get_Fitness_Value(self,fitfunct,choro):
+
+    def Get_Fitness_Value(self, fitfunct, choro):
         return fitfunct(choro)
+
+
+def main():
+    # just for testing
+    bga1 = BGA(target_function=lambda x: x ** 2, function_dim=2, population=10, crossover_rate=0.8, mutation_rate=0.2,
+               chromosome_bits=8)
+    bga1.Random_Chromosome()
+    print(bga1.population_matrix)
+
+
+if __name__ == "__main__":
+    main()
