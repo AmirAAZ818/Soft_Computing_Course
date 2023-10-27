3ba0d6986dd92ad1f42220fceb190cf35800f161
Sadra Koocheckzadeh <141414480+Sadrakch@users.noreply.github.com>
2023-10-27 11:03:30 +0330

    Delete BGA_HW_1/griewank.py
    To merge with a main problem.py file

--- BGA_HW_1/griewank.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def griewank(x):
-    n = len(x)
-    s = sum([xi**2 for xi in x]) / 4000
-    p = math.prod([math.cos(xi / math.sqrt(i + 1)) for i, xi in enumerate(x)])
-    return 1 + s - p
-# it is griewank function
