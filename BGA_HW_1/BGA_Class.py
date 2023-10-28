8a13bf0c628f2c775e259a0da337c66fab1fef7e
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-10-28 20:02:59 +0330

    a litttle change

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -14,6 +14,7 @@ class BGA:
         self.pm = mutation_rate
 
 
+<<<<<<< Updated upstream
     def Random_Choromosom(self):
         population_matrix = []
         for i in range(self.Population):
@@ -41,6 +42,8 @@ class BGA:
         self.Child_matrix = child_matrix
 
 
+=======
+>>>>>>> Stashed changes
     def Mutation(self, child_matrix):
         rand = random()
         if rand > self.pm :
