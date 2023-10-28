1888a4a210f6e69315e99cc6210391f1a4c9fd82
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-10-28 22:30:10 +0330

    Made a method to count bit len of vars

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -1,21 +1,28 @@
 from random import randint, random
+import math
 
 
 # class for BGA Algo
 
 class BGA:
     def __init__(self, max_gen, target_function, function_dim, population,
-                 crossover_rate, mutation_rate, precision, chromosome_bits=8, ):
+                 crossover_rate, mutation_rate, precision, function_config):
 
+        self.func_config = function_config  # a list of dicts containing boundary of each dimension : [{"low": a, "high":b}, ...]
         self.population_matrix = None
         self.function = target_function  # تابع هدف
-        self.parameters = function_dim  # تعداد پارامترهای تابع
+        self.dim = function_dim  # تعداد پارامترهای تابع
         self.population = population  # اندازه جمعیت
-        self.chromosome_len = chromosome_bits
+        self.chromosome_len = 0
         self.pc = crossover_rate
         self.pm = mutation_rate
         self.max_gen = max_gen  # maximum number of generations
         self.precision = precision  # quantization error
+        self.L = [0 for i in range(self.dim)]
+
+    def Run(self):
+        self.get_len_chro()  # estimating the number of bits for each dimension.
+        self.Random_population()  # Making random population
 
     def Random_population(self):
         population_matrix = []  # N x Sigma(Li) from i = 0 to function_dim
@@ -28,7 +35,23 @@ class BGA:
 
         self.population_matrix = population_matrix
 
-    def
+    def get_len_chro(
+            self):  # a method that values the length list: L. where L[i] means the length of the bits used for the i'th variable.
+
+        # private method
+        def var_bit_len(boundary,
+                        idx):  # boundary is a dictionary : bound = {"low": a, "high": b}, output : appending the bit len of the corresponding variable in the L list
+            # we use precision and its formula to estimate bit len of the variable
+            rng = boundary['high'] - boundary['low']
+            length = math.ceil(math.log(rng / self.precision, 2) - 1)
+            print()
+            self.L[idx] = length
+
+        boundaries = self.func_config  # giving the config to a variable called boundaries
+        for var_idx in range(len(boundaries)):  # giving each boundary to the method we made and updating the L list
+            var_bit_len(boundaries[var_idx], var_idx)
+
+        self.chromosome_len = sum(self.L)
 
     def Crossover(self, parent_matrix):
         child_matrix = []
@@ -63,10 +86,14 @@ class BGA:
 def main():
     # just for testing
     bga1 = BGA(target_function=lambda x, y: x ** 2 + y ** 2, function_dim=2, population=10, crossover_rate=0.8,
-               mutation_rate=0.2,
-               chromosome_bits=8)
-    bga1.Random_Chromosome()
-    print(bga1.population_matrix)
+               mutation_rate=0.2, max_gen=50, precision=0.1, function_config=[{'low': 2, 'high': 5}, {'low': -6, 'high': 0}])
+    # print(bga1.population_matrix)
+    #
+    # bga1.Run()
+    #
+    # print(bga1.population_matrix)
+    # print(bga1.L)
+    # print(bga1.chromosome_len)
 
 
 if __name__ == "__main__":
