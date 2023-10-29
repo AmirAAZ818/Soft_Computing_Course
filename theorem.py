b7dcc1b24efd17b9268b2e05e844dcb32a7195ae
Sadra Koocheckzadeh <141414480+Sadrakch@users.noreply.github.com>
2023-10-29 14:02:37 +0330

    Update theorem.py

--- theorem.py
+++ theorem.py
@@ -1,4 +1,5 @@
-from math import cos,sqrt,sin,pi,prod,exp
+from math import cos,sqrt,sin,pi,prod,exp,tan
+import numpy
 #-------------------------------------------------------------------------
 # The two Demensional Griewank's function
 
@@ -38,7 +39,7 @@ def rosenbrock(x, y):
     b = 100
     return (a - x) ** 2 + b * (y - x ** 2) ** 2
 #--------------------------------------------------------
-def fitness_function(chromosome):
+def fitness_function_1(chromosome):
     
     x = chromosome[0]
     y = chromosome[1]
@@ -46,24 +47,15 @@ def fitness_function(chromosome):
     fitness = - (x**2 + y**2 + z**2)
     return fitness
 #---------------------------------------------------------
-def fitness_function(chromosome):
+def fitness_function_2(chromosome):
     
     x = chromosome[0]
     y = chromosome[1]
     z = chromosome[2]
-    fitness = math.sin(x) + math.cos(y) + math.tan(z)
+    fitness = sin(x) + cos(y) + tan(z)
     return fitness
 #------------------------------------------------------
-def fitness_function(chromosome):
-    
-    x = chromosome[0]
-    y = chromosome[1]
-    z = chromosome[2]
-    fitness = math.sin(x) + math.cos(y) + math.tan(z)
-    return fitness
-#-------------------------------------------------------
-
-def fitness_function(chromosome):
+def fitness_function_3(chromosome):
     
     x = chromosome[0]
     y = chromosome[1]
@@ -83,30 +75,9 @@ def fitness_function(chromosome):
     fitness = obj - penalty
     return fitness
 #----------------------------------------------------------------------------
-
-def f(chro1, chro2):
+def fitness_function_4(chro1, chro2):
     value = (1 + cos(2 * pi * chro1 * chro2)) * exp(- (abs(chro1) + abs(chro2)) / 2)
     return value
-#--------------------------------------------------------------------------
-def f(chromosome):
-    x = chromosome[0]
-    y = chromosome[1]
-    z = chromosome[2]
-    w = chromosome[3]
-    obj = x * sin(y) + y * cos(z) + z * sin(w) + w * cos(x)
-    penalty = 0
-    # check if any constraint is violated and add a penalty accordingly
-    if x + y + z + w > 10:
-        penalty += (x + y + z + w - 10)**2
-    if x - z < 0:
-        penalty += (x - z)**2
-    if y - w < 0:
-        penalty += (y - w)**2
-    if x < 0 or y < 0 or z < 0 or w < 0:
-        penalty += (x**2 + y**2 + z**2 + w**2)
-    # fitness score is the objective function value minus the penalty term
-    fitness = obj - penalty
-    return fitness
 #----------------------------------------------------------------------------------
 # Booth function
 
@@ -118,12 +89,12 @@ def booth(x):
 def bukin_n6(x):
     return 100 * np.sqrt(np.abs(x[1] - 0.01*x[0]**2)) + 0.01*np.abs(x[0] + 10)
 #-------------------------------------------------------------------------------
-# Cross-in-Italy function
+# Cross-in-Tray function
 
-def cross_in_italy(x):
+def cross_in_tray(x):
     return -0.0001 * (np.abs(np.sin(x[0]) * np.sin(x[1]) * np.exp(np.abs(100 - np.sqrt(x[0]**2 + x[1]**2)/np.pi))) + 1)
 #-----------------------------------------------------------------------------------------------------------------------------
-# Holder table function
+# Holder_Table function
 
 def holder_table(x):
     num = -np.abs(np.sin(x[0]) * np.cos(x[1]) * np.exp(np.abs(1 - np.sqrt(x[0]**2 + x[1]**2)/np.pi))
@@ -131,25 +102,29 @@ def holder_table(x):
     den = 1 + np.abs(x[0] + x[1]) * np.exp(np.abs(1 - np.sqrt(x[0]**2 + x[1]**2)/np.pi))
     return num / den
 #----------------------------------------------------------------------------------------------------
-# McComick function
+# McCormick function
 
 def mccormick(x):
     return np.sin(x[0] + x[1]) + (x[0] - x[1])**2 - 1.5*x[0] + 2.5*x[1] + 1
-#-----------------------------------------------------------------------------
-# Schaffer function N.1
-
-def schaffer_n1(x):
-    return x**2
-#------------------------------
-# Schaffer function N.2
-
-def schaffer_n2(x):
-    return x**2
 #------------------------------
 # Poloni's two objective function
-
-def polonis_two_objective(x):
-    f1 = (1 - np.exp(-(x[0]-1)**2 - (x[1]+1)**2)) / 0.45
-    f2 = (2.0 * (x[0]-0.5)**2 + 2.0 * (x[1]-0.5)**2)
-    return f1, f2
+def poloni(x):
+    
+    A1 = 0.5 * sin(1) - 2 * cos(1) + sin(2) - 1.5 * cos(2)
+    A2 = 1.5 * sin(1) - cos(1) + 2 * sin(2) - 0.5 * cos(2)
+    B1 = 0.5 * sin(x[0]) - 2 * cos(x[0]) + sin(x[1]) - 1.5 * cos(x[1])
+    B2 = 1.5 * sin(x[0]) - cos(x[0]) + 2 * sin(x[1]) - 0.5 * cos(x[1])
+    
+    f1 = [1 + (A1 - B1)**2 + (A2 - B2)**2]
+    f2 = [(x[0] + 3)**2 + (x[1] + 1)**2]
+    return [f1, f2]
 #-----------------------------------------------------------
+# Viennet function
+def viennet(x):
+    
+    f1 = 0.5 * (x[0]**2 + x[1]**2) + np.sin(x[0]**2 + x[1]**2)
+    f2 = ((3*x[0] - 2*x[1] + 4)/8)**2 + ((x[0] - x[1] + 1)/27)**2 + 15
+    f3 = 1 / (x[0]**2 + x[1]**2 + 1) - 1.1 * exp(-x[0]**2 - x[1]**2)
+    
+    return [f1, f2, f3]
+#-------------------------------------------------------------------------
