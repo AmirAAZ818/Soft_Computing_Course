8fa08afa87800ecd8b1d3e196c740d19cc21845f
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-11-01 21:22:49 +0330

    added best so far field. changed some of the methods

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -23,29 +23,31 @@ class BGA:
         self.precision = precision  # quantization error
         self.L = [0 for i in range(self.dim)]
         self.history = {"avg_fitness": [], "best_so_far": []}
-        self.best_so_far = 0
+        self.best_so_far = {'fitness': 0, "chromosome": list()}
 
     def Run(self):
         print("BGA Is Running .....")
         # print("_________Parameters_________")
 
-
         self.get_len_chro()  # estimating the number of bits for each dimension.
         self.Random_population()  # Making random population
         # debug
-        print("Random Population Matrix is __________")
+        print("__________ Random Population Matrix __________")
         print(self.population_matrix)
-        print("L is _______")
+        print("_______ L _______")
         print(self.L)
 
         for generation in range(self.max_gen):
             print(f"________________ Generation : {generation} ________________")
             self.one_gen()
 
-        print("________Plotting________")
+        print("___________ Results ___________")
+        print(f"Best Chromosome :        {self.decode_chromosome(self.best_so_far['chromosome'])}")
+        print(f"Best Fitness    :        {self.best_so_far['fitness']}")
+        print(f"Average Fitness :        {self.history['avg_fitness'][self.last_gen - 1]}")
+
+        # plotting Results
         self.plot_info()
-
-
 
     def one_gen(self):
 
@@ -55,14 +57,12 @@ class BGA:
         mating_pool = self.roulette_wheel(fitness_values)
         next_gen_v1 = self.Crossover(mating_pool)
         nex_gen = self.Mutation(next_gen_v1)
-        print(f"new gen is : {nex_gen}")
+        print(f"Population : {nex_gen}")
         self.population_matrix = nex_gen
         self.last_gen += 1
 
-
-
-
-
+    def best_chromosome(self):
+        return self.best_so_far['chromosome']
 
     def roulette_wheel(self,
                        fitness_values):  # inputs fitness values of each chromosome, returns the mating pool Matrix of size N x L
@@ -92,10 +92,11 @@ class BGA:
         return mating_pool
 
     def log(self, fitness_list):  # saves the best so far and avg fitness to log
-        if self.best_so_far < max(fitness_list):
-            self.best_so_far = max(fitness_list)
+        if self.best_so_far['fitness'] < max(fitness_list):
+            self.best_so_far['fitness'] = max(fitness_list)
+            self.best_so_far['chromosome'] = self.population_matrix[fitness_list.index(self.best_so_far['fitness'])]
 
-        self.history["best_so_far"].append(self.best_so_far)
+        self.history["best_so_far"].append(self.best_so_far['fitness'])
         self.history["avg_fitness"].append(sum(fitness_list) / len(fitness_list))
 
     def Random_population(self):
@@ -127,50 +128,51 @@ class BGA:
 
         self.chromosome_len = sum(self.L)
 
-    def decode_chromosomes(self):  # returns a tuple of tuples of the decoded value for variables, size: N x dim
+    def decode_chromosome(self,  # A method for decoding a chromosome
+                          chromosome):  # returns a tuple containing the real values of the genes for a given chromosome
+
         def binary_to_decimal(
                 binary_num):  # binary_num is a list of 0s and 1s. we join it and make it string, and then we convert it using a special way in python
             b_num = list(map(str, binary_num))
             return int(''.join(b_num), 2)
 
-        def decode_chromosome(
-                chromosome):  # returns a tuple containing the real values of the genes for a given chromosome
-            rng = 0
-            decoded_chromosome = []
-            for gene_idx, length in enumerate(self.L):
-                # print(f"________________________bit len of L_{gene_idx} is : {length}_____________________________")
-                # converting from binary to decimal
-                # print("$$$$$$$$$$$Binary to decimal phase$$$$$$$$$$$$")
-                new_rng = rng + length
+        rng = 0
+        decoded_chromosome = []
+        for gene_idx, length in enumerate(self.L):
+            # print(f"________________________bit len of L_{gene_idx} is : {length}_____________________________")
+            # converting from binary to decimal
+            # print("$$$$$$$$$$$Binary to decimal phase$$$$$$$$$$$$")
+            new_rng = rng + length
 
-                x_i = chromosome[rng:new_rng]  # slice of the list that contains the gene bits
-                # print(f"sliced chromosome : {x_i}")
-                decoded_gene = binary_to_decimal(x_i)  # decoded_gene = decoded binary number
-                # print(f"decimal converted gene  : {decoded_gene}")
+            x_i = chromosome[rng:new_rng]  # slice of the list that contains the gene bits
+            # print(f"sliced chromosome : {x_i}")
+            decoded_gene = binary_to_decimal(x_i)  # decoded_gene = decoded binary number
+            # print(f"decimal converted gene  : {decoded_gene}")
 
-                # normalizing the decoded gene value
-                # print("$$$$$$$ Normalizing phase $$$$$$$$")
-                no_decoded_gene = decoded_gene / ((2 ** length) - 1)
-                # print(f"normalized gene : {no_decoded_gene}")
+            # normalizing the decoded gene value
+            # print("$$$$$$$ Normalizing phase $$$$$$$$")
+            no_decoded_gene = decoded_gene / ((2 ** length) - 1)
+            # print(f"normalized gene : {no_decoded_gene}")
 
-                # next step (don't know it in english :) )
-                # print("$$$$$$$$$$ Negasht phase $$$$$$$")
-                config = self.func_config[gene_idx]  # assigning the boundary of the gene to a variable called config
-                # print(f"dim config is : {config}")
-                x_real = config["low"] + ((config['high'] - config['low']) * no_decoded_gene)
-                # print(f"x real is : {x_real}")
-                decoded_chromosome.append(x_real)
+            # next step (don't know it in english :) )
+            # print("$$$$$$$$$$ Negasht phase $$$$$$$")
+            config = self.func_config[gene_idx]  # assigning the boundary of the gene to a variable called config
+            # print(f"dim config is : {config}")
+            x_real = config["low"] + ((config['high'] - config['low']) * no_decoded_gene)
+            # print(f"x real is : {x_real}")
+            decoded_chromosome.append(x_real)
 
-                rng = new_rng
+            rng = new_rng
 
-            return tuple(decoded_chromosome)
+        return tuple(decoded_chromosome)
 
+    def decode_chromosomes(self):  # returns a tuple of tuples of the decoded value for variables, size: N x dim
         # giving each chromosome to the decode_chromosome function and adding it in a list
         decoded_values = []  # this will be converted to a tuple at the end
         for chro in self.population_matrix:
             # print("_________________________________________________")
             # print(f"Current chromosome is : {chro}")
-            decoded_values.append(decode_chromosome(chro))
+            decoded_values.append(self.decode_chromosome(chro))
 
         return tuple(decoded_values)
 
@@ -199,12 +201,13 @@ class BGA:
 
         return child_matrix
 
-
     def Mutation(self, child_matrix):  # input: a matrix of childs from cross over stage / output: mutated child_matrix
         # this function gets a matrix of childs, this matrix is output of cross over stage
         # if rand is less than pm, changes 1 bit of one random choromosom 
-        mutation_mat = [[1 if random() <= self.pm else 0 for i in range(self.chromosome_len)] for j in range(self.population)]
-        mutated_matrix = [[bit1 ^ bit2 for bit1, bit2 in zip(mutation_mat[i], child_matrix[i])] for i in range(self.population)]
+        mutation_mat = [[1 if random() <= self.pm else 0 for i in range(self.chromosome_len)] for j in
+                        range(self.population)]
+        mutated_matrix = [[bit1 ^ bit2 for bit1, bit2 in zip(mutation_mat[i], child_matrix[i])] for i in
+                          range(self.population)]
 
         return mutated_matrix
 
@@ -225,26 +228,25 @@ class BGA:
         y = self.history['avg_fitness']
         x = list(range(self.max_gen))
         axis[0].plot(x, y, label="Average Fitness", color="blue")
+        axis[0].legend()
 
         # Plot of the best fitness so far
         y = self.history['best_so_far']
         x = list(range(self.max_gen))
         axis[1].plot(x, y, label="Best So Far", color="blue")
-        plt.legend()
+        axis[1].legend()
+
         plt.show()
         # plt.savefig(r"E:\University of Kerman\Term 5\Soft Computing\HomeWorks\Soft_Computing_Course\BGA_HW_1\Plots\BGA_plots.png")
 
 
-
-
+# max of fGriwank is 181.64 in the given bounds
 def main():
     # just for testing
-    bga1 = BGA(target_function=theorem.fGriewank, function_dim=2, population=200, crossover_rate=0.8,
-               mutation_rate=0.1, max_gen=200, precision=0.01,
+    bga1 = BGA(target_function=theorem.fGriewank, function_dim=2, population=150, crossover_rate=0.8,
+               mutation_rate=0.1, max_gen=70, precision=0.01,
                function_config=[{'low': -600, 'high': 600}, {'low': -600, 'high': 600}],
-               fitness_function=theorem.fGriewank)
-
-
+               fitness_function=lambda x: 182 - theorem.fGriewank(x))
 
     # print(bga1.population_matrix)
     #
@@ -270,7 +272,6 @@ def main():
     # print(mutation_mat)
     # print(child_matrix)
     # print(mutated_matrix)
-
 
 
 if __name__ == "__main__":
