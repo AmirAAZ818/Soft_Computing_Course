b9c228d71710d6d07ee611f1e9694fb384f90f62
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-11-04 20:32:33 +0330

    adding a save dir argument to save the plot in the given dir

--- BGA_HW_1/main.py
+++ BGA_HW_1/main.py
@@ -1,33 +1,30 @@
 from BGA_Class import BGA
 import theorem
 
+
 def main():
     """
     you can run the algorithm for the function provided and the functions that you want.
     make sure to give the function to the constructor in the required format.
     NOTE: you can Start the algorithm by calling BGA_object.Run() or GA_object.Run()
+    Warnin
     """
 
     # mccormick function
-    mcc_bga = BGA(target_function=theorem.mccormick, function_dim=2, population=500, crossover_rate=0.8,
-               mutation_rate=0.005, max_gen=80, error=0.001,
-               function_config=[{'low': -1.5, 'high': 4}, {'low': -3, 'high': 4}],
-               fitness_function=lambda x: 45 - theorem.mccormick(x), run_bga=5)
+    # mcc_bga = BGA(target_function=theorem.mccormick, function_dim=2, population=500, crossover_rate=0.8,
+    #            mutation_rate=0.005, max_gen=80, error=0.001,
+    #            function_config=[{'low': -1.5, 'high': 4}, {'low': -3, 'high': 4}],
+    #            fitness_function=lambda x: 45 - theorem.mccormick(x), run_bga=5)
 
     # mcc_bga.Run()
 
     # Rastrigin function
-    rast_bga = BGA(target_function=theorem.rastrigin, function_dim=2, population=600, crossover_rate=0.8,
-               mutation_rate=0.005, max_gen=150, error=0.001,
-               function_config=[{'low': -5.12, 'high': 5.12}, {'low': -5.12, 'high': 5.12}],
-               fitness_function=lambda x: 85 - theorem.rastrigin(x), run_bga=5)
+    rast_bga = BGA(target_function=theorem.rastrigin, function_dim=4, population=600, crossover_rate=0.8,
+                   mutation_rate=0.005, max_gen=250, error=0.001,
+                   function_config=[{'low': -5.12, 'high': 5.12}, {'low': -5.12, 'high': 5.12},{'low': -5.12, 'high': 5.12}, {'low': -5.12, 'high': 5.12}],
+                   fitness_function=lambda x: 85 - theorem.rastrigin(x), run_bga=5)
 
-    # rast_bga.Run()
-
-
-
-
-
+    rast_bga.Run()
 
 
 if __name__ == "__main__":
