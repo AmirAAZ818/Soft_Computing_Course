b65b7565f9169582374cc7285181f14ce380e2e7
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-11-04 18:47:24 +0330

    making changed to run algorithm multiple times
    added some methods and comments to make it easier to read and understand

--- BGA_HW_1/BGA_Class.py
+++ BGA_HW_1/BGA_Class.py
@@ -2,14 +2,14 @@ from random import randint, random
 import theorem
 import math
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from tabulate import tabulate
 
 
 class BGA:
 
-    def __init__(self, max_gen, target_function, fitness_function, function_dim, population,
-                 crossover_rate, mutation_rate, precision, function_config):
+    def __init__(self, target_function, fitness_function, function_dim, population,
+                 crossover_rate, mutation_rate, precision, function_config, max_gen=50, run_bga=30):
 
         self.func_config = function_config  # a list of dicts containing boundary of each dimension : [{"low": a, "high":b}, ...]
         self.population_matrix = None
@@ -22,22 +22,25 @@ class BGA:
         self.pm = mutation_rate
         self.max_gen = max_gen  # maximum number of generations
         self.last_gen = 0
-        self.precision = precision  # quantization error
+        self.precision = precision  # quantization error # todo
         self.L = [0 for i in range(self.dim)]
-        self.history = {"avg_fitness": [], "best_so_far": []}
+        self.history = {"mean_fitness": [], "best_so_far": [], "avg_mean_fitness": [], "avg_best_so_far": []}
         self.best_so_far = {'fitness': 0, "chromosome": list()}
         self.best_current = {'fitness': 0, "chromosome": list()}
+        self.runs = run_bga
+        self.best_answers = []  # best decoded value of the chromosomes found in each full run of the algorithm
 
     def print_parameters(self):
         # This is a method that prints parameters in tabular structure
         parameters = [
+            ["Algorithm Runs", self.runs],
+            ["Maximum Generation", self.max_gen],
             ["Population Size", self.population],
             ["Precision", self.precision],
             ["CrossOver Rate", self.pc],
             ["Mutation Rate", self.pm],
             ["Chromosome Length", self.chromosome_len],
             ["Input Bit Length", self.L],
-            ["Max Gen", self.max_gen],
             ["Function Input Shape", self.dim]
         ]
         print(tabulate(parameters, headers=["Parameter", "Value"], tablefmt="fancy_grid"))
@@ -50,49 +53,108 @@ class BGA:
 
         # First step
         self.get_len_chro()  # estimating the number of bits for each dimension.
-        self.Random_population()  # Making random population
 
         # printing the params in tabular structure
         self.print_parameters()
 
         print("______________________________________")
 
-        pbar = tqdm(range(self.max_gen), colour="green")
-        for generation in pbar:
-            self.one_gen()
+        for run in range(self.runs):
+            # Reset the properties that were given values for one fully run of the algorithm
+            self.reset()
 
-            pbar.set_postfix(
-                {"Generation": generation})
-        print("______________________________________")
+            # Fully running the algorithm
+            print(f"Run : {run + 1}")
+            self.one_run()
 
+            # Logging lists of best_so_far and mean fitness produced by one_run().
+            self.log_algo()
+
+        solution, opt = self.post_process()
         # Showing results
-        print("___________ Results ___________")
-        print(f"Best So Far Chromosome :        {self.best_so_far['chromosome']}")
-        print(f"Best So Far Fitness    :        {self.best_so_far['fitness']}")
-        print(f"Average Fitness        :        {self.history['avg_fitness'][self.last_gen - 1]}")
-        print(f"Best Last Fitness      :        {self.best_current['fitness']}")
-        print(f"Best Last Chromosome   :        {self.best_current['chromosome']}")
-        print(f"Best Last X            :        {self.decode_chromosome(self.best_current['chromosome'])}")
-        print(
-            f"Optimum Value          :        {self.function(self.decode_chromosome(self.best_current['chromosome']))}")
+        print("________________________ Results ________________________")
+        print(f"Average Best Solution     :        {solution}")
+        print(f"Optimum Value             :        {opt}")
+        print(f"Last Average Best So Far  :        {self.history['avg_best_so_far'][self.last_gen - 1]}")
+        print(f"Last Average Mean Fitness :        {self.history['avg_mean_fitness'][self.last_gen - 1]}")
 
-        # plotting Results
+
+        # Plotting Results
         self.plot_info()
 
-    def one_gen(self):
+    def post_process(self):
+        """This method processes self.history['avg_mean_fitness'] and self.history['avg_best_so_far'] and self.best_answers'}
+        returns:
+            1. Mean Best Solution (mean of the best answers found in each run)
+            2. Optimum value found for the target function
+        """
+
+        # Evaluating best solution
+        mean_best_solutions = [0 for i in range(self.dim)]
+
+        for i in range(self.dim):
+            _sum = 0
+            for j in range(self.runs):
+                _sum += self.best_answers[j][i]
+
+            mean_best_solutions[i] = _sum/len(self.best_answers)
+
+        # avg best so far, and avg mean fitness
+        avg_mean_fitness = [0 for i in range(self.max_gen)]
+        avg_best_so_far = [0 for i in range(self.max_gen)]
+
+        for i in range(self.max_gen):
+            for j in range(self.runs):
+                avg_mean_fitness[i] += self.history['avg_mean_fitness'][j][i]
+                avg_best_so_far[i] += self.history['avg_best_so_far'][j][i]
+            avg_mean_fitness[i] = avg_mean_fitness[i] / self.runs
+            avg_best_so_far[i] = avg_best_so_far[i] / self.runs
+
+        self.history['avg_mean_fitness'] = avg_mean_fitness
+        self.history['avg_best_so_far'] = avg_best_so_far
+
+        # Optimum found
+        opt = self.function(mean_best_solutions)
+
+        return tuple(mean_best_solutions), opt
+
+    def reset(self):
+        """This method empties the lists and values
+        used for tracking the metrics for one fully run of the algorithm"""
+        self.history['mean_fitness'] = []
+        self.history['best_so_far'] = []
+
+        self.best_so_far['fitness'] = 0
+        self.best_so_far['chromosome'] = []
+
+        self.best_current['fitness'] = 0
+        self.best_current['chromosome'] = []
+
+        self.last_gen = 0
+
+    def one_run(self):  # This method fully runs the algorithm once
+        self.Random_population()  # Making random population
+
+        # running the algorithm
+        for generation in trange(self.max_gen):
+            self.one_gen()
+
+        # Saving fitness values of the last produced population
+        decoded_population = self.decode_chromosomes()
+        fitness_values = self.get_Fitness(decoded_chromosomes=decoded_population)
+        self.log_gen(fitness_values)
+
+    def one_gen(self):  # This method produces one generation of the algorithm
 
         decoded_population = self.decode_chromosomes()
         fitness_values = self.get_Fitness(decoded_chromosomes=decoded_population)
-        self.log(fitness_values)
+        self.log_gen(fitness_values)  # log the info of the last generation
         mating_pool = self.roulette_wheel(fitness_values)
         next_gen_v1 = self.Crossover(mating_pool)
         nex_gen = self.Mutation(next_gen_v1)
         # print(f"Population : {nex_gen}")
         self.population_matrix = nex_gen
         self.last_gen += 1
-
-    def best_chromosome(self):
-        return self.decode_chromosome(self.best_so_far['chromosome'])
 
     def roulette_wheel(self,
                        fitness_values):  # inputs fitness values of each chromosome, returns the mating pool Matrix of size N x L
@@ -121,7 +183,7 @@ class BGA:
 
         return mating_pool
 
-    def log(self, fitness_list):  # saves the best so far and avg fitness to log
+    def log_gen(self, fitness_list):  # saves the best so far and avg fitness of a generation in one run of BGA
         # updating best so far property
         if self.best_so_far['fitness'] < max(fitness_list):
             self.best_so_far['fitness'] = max(fitness_list)
@@ -133,7 +195,15 @@ class BGA:
 
         # Saving history
         self.history["best_so_far"].append(self.best_so_far['fitness'])
-        self.history["avg_fitness"].append(sum(fitness_list) / len(fitness_list))
+        self.history["mean_fitness"].append(sum(fitness_list) / len(fitness_list))
+
+    def log_algo(self):
+        self.history["avg_mean_fitness"].append(self.history['mean_fitness'])
+        self.history["avg_best_so_far"].append(self.history['best_so_far'])
+
+        # Saving the best decoded value of the chromosome of the last generated population
+        decoded_best_last_chromosome = self.decode_chromosome(chromosome=self.best_current['chromosome'])
+        self.best_answers.append(decoded_best_last_chromosome)
 
     def Random_population(self):
         population_matrix = []  # N x Sigma(Li) from i = 0 to function_dim
@@ -259,38 +329,32 @@ class BGA:
     def plot_info(self):
 
         plt.style.use('Solarize_Light2')
-        fig, axis = plt.subplots(1, 2)
 
         # Plot of avg fitness
-        y2 = self.history['best_so_far']
-        y1 = self.history['avg_fitness']
+        y2 = self.history['avg_best_so_far']
+        y1 = self.history['avg_mean_fitness']
         x = list(range(self.max_gen))
 
-        axis[0].plot(x, y1, label="Average Fitness", color="blue")
-        axis[0].plot(x, y2, label="Best So Far", color="red", ls="--",)
-        axis[0].legend()
+        plt.plot(x, y1, label="Average Mean Fitness", color="blue")
+        plt.plot(x, y2, label="Average Best So Far", color="red", ls="--")
 
-        # Plot of the best fitness so far
-        # y3 =
-        # axis[1].plot(x, y, label="Best So Far", color="blue")
-
-        # axis[1].legend()
-
+        plt.legend()
         plt.show()
         # plt.savefig(r"E:\University of Kerman\Term 5\Soft Computing\HomeWorks\Soft_Computing_Course\BGA_HW_1\Plots\BGA_plots.png")
 
 
 def main():
     # just for testing
-    bga1 = BGA(target_function=theorem.mccormick, function_dim=2, population=1000, crossover_rate=0.8,
+    bga1 = BGA(target_function=theorem.mccormick, function_dim=2, population=500, crossover_rate=0.8,
                mutation_rate=0.005, max_gen=80, precision=0.001,
                function_config=[{'low': -1.5, 'high': 4}, {'low': -3, 'high': 4}],
-               fitness_function=lambda x : 45 - theorem.mccormick(x))
+               fitness_function=lambda x: 45 - theorem.mccormick(x), run_bga=10)
 
     # print(182 - theorem.fGriewank([28, 0]))
     # print(theorem.fGriewank([-33, -43]))
 
     bga1.Run()
+
 
 if __name__ == "__main__":
     main()
