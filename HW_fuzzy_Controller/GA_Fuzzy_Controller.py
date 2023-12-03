bfe076ba71d0f1a23c6f0f7863278fdacdc381fb
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-12-04 00:47:04 +0330

    implemented pm fizifier with mf makers

--- HW_fuzzy_Controller/GA_Fuzzy_Controller.py
+++ HW_fuzzy_Controller/GA_Fuzzy_Controller.py
@@ -1,60 +1,54 @@
 import numpy as np
+import Membership_functions as mfs
 
 
 class Fuzzy_Controller:
 
-    def __init__(self, population,max_gen, k):
+    def __init__(self, population, max_gen, k):
         self.population = population
         self.N = max_gen
         self.k = k
         self.prev_bsf = 0
         self.prev_pm = 0
-    def fuzzifier(self, cur_gen, p_m, cur_bsf ):
+
+    def fuzzifier(self, cur_gen, p_m, cur_bsf):
+        membership_values = {"cm": None, "pm_prev": None}
         if cur_gen % self.k != 0:
             return
 
-        def CM(self, cur_bsf):
-            cm = self.prev_bsf / cur_bsf
+        def CM(cur_bsf):
+            cm = self.prev_bsf / cur_bsf + 1e-3
             return cm
 
         cm = CM(cur_bsf)
 
+        membership_values["pm_prev"] = self.fuzzify_pm(p_m)
 
+    def fuzifify_generation(self, gen):
 
-
-
-
-
-
-
-
-
-    def fuzifify_generation(self,gen):
-        
         """ this method gets the number of current generations
             and calculate the gen / max gen and fuzzify the result.
             it returns the result as a dictionary with three keys (start, middle, end)
             these keys are the labels of fuzzy sets """
-        
+
         x = gen / self.N
         # for range 0 to 0.4 : start is 1 else is 0
-        if x < 0.4 :
-            return {'start':1, 'middle':0, 'end':0}
-        
+        if x < 0.4:
+            return {'start': 1, 'middle': 0, 'end': 0}
+
         # for range 0.4 to 0.6 : start is -5x+3, middle is 5x-2, end is 0
         elif x < 0.6:
-            return {'start':(-5*x)+3, 'middle':(5*x)-2, 'end':0}
-        
+            return {'start': (-5 * x) + 3, 'middle': (5 * x) - 2, 'end': 0}
+
         # for range 0.6 to 0.8 : start is 0, middle is -5x+4, end is 5x-3
         elif x < 0.8:
-            return {'start':0, 'middle':(-5*x)+4, 'end':(5*x)-3}
-        
+            return {'start': 0, 'middle': (-5 * x) + 4, 'end': (5 * x) - 3}
+
         # for range 0.8 to 1 : end is 1 else is 0
         else:
-            return {'start':0, 'middle':0, 'end':1}
-        
+            return {'start': 0, 'middle': 0, 'end': 1}
 
-    def fuzzify_cm(self, current_bst, last_best,maximize=False):
+    def fuzzify_cm(self, current_bst, last_best, maximize=False):
 
         """ the set for cm in minimize and maximize is diffrent.
             to slve this problem and use uniq sets, we use diffrent formula 
@@ -64,28 +58,23 @@ class Fuzzy_Controller:
         cm = current_bst / last_best
         if maximize:
             cm = 1 / cm
-        
+
         if cm < 0.7:
-            return {'low':1, 'high':0}
+            return {'low': 1, 'high': 0}
         else:
-            return {'low':(3.33 * cm) - 3.33, 'high':(3.33 * cm) -2.33}
+            return {'low': (3.33 * cm) - 3.33, 'high': (3.33 * cm) - 2.33}
 
+    def fuzzify_pm(self, p_m):
+        low_mf = mfs.trapmf_maker([0, 1], 0, [0, 1e-3], 5e-3)
+        avg_mf = mfs.trimf_maker([0, 1], 1e-3, 5e-3, 1e-2)
+        high_mf = mfs.trapmf_maker([0, 1], 5e-3, [1e-2, 15e-3], 15e-3)
 
-    def fuzzify_lastpm(self,lastpm):
+        return {"low": low_mf(p_m), "avg": avg_mf(p_m), "high": high_mf(p_m)}
 
-        if lastpm < 0.001:
-            return {'low':1, 'aveage':0, 'high':0}
-        elif lastpm < 0.005:
-            return {'low':(-250*lastpm)+1.25, 'aveage':(250*lastpm)-0.25, 'high':0}
-        elif lastpm < 0.01:
-            return {'low':0, 'aveage':(-200*lastpm)+2, 'high':(200*lastpm)-1}
-        else:
-            return {'low':0, 'aveage': 0, 'high':1}
-        
 
 # testing 
 def main():
-    fuzzy = Fuzzy_Controller(10,100)
+    fuzzy = Fuzzy_Controller(10, 100)
     gen = 10
     x = fuzzy.fuzifify_generation(gen)
     print(f'fuzzified generation for gen {gen} and max gen {fuzzy.N} ', x)
@@ -98,7 +87,6 @@ def main():
     pm = 0.002
     z = fuzzy.fuzzify_lastpm(pm)
     print(f'fuzzified pm for pm {pm} ', z)
-
 
 
 if __name__ == "__main__":
--- HW_fuzzy_Controller/main.py
+++ HW_fuzzy_Controller/main.py
@@ -4,20 +4,26 @@ import matplotlib.pyplot as plt
 from Membership_functions import trimf_maker, trapmf_maker
 import numpy as np
 
+
 def main():
     # for test
-    mf1 = trimf_maker([0, 10], 2, 5, 8)
-    mf2 = trapmf_maker([0,float("inf")], 0,[0,10], 10)
-    x = np.arange(0, 20, 1)
-    Y = [mf2(x[i]) for i in range(len(x))]
+    low_mf = trapmf_maker([0, 15e-3], 0, [0, 1e-3], 5e-3)
+    avg_mf = trimf_maker([0, 15e-3], 1e-3, 5e-3, 1e-2)
+    high_mf = trapmf_maker([0, 15e-3], 5e-3, [1e-2, 15e-3], 15e-3)
+    x = np.arange(0, 15e-3, 1e-4)
+    y1 = [low_mf(x[i]) for i in range(len(x))]
+    y2 = [avg_mf(x[i]) for i in range(len(x))]
+    y3 = [high_mf(x[i]) for i in range(len(x))]
 
-
-    plt.plot(x, Y)
-    plt.title('trimf')
+    plt.plot(x, y1, color="blue")
+    plt.plot(x, y2, color="green")
+    plt.plot(x, y3, color="red")
+    plt.title('P_m')
     plt.xlabel('x')
     plt.ylabel('Degree of Membership')
-    # plt.ylim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
     plt.show()
+
 
 if __name__ == "__main__":
     main()
