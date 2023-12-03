c494d477e9c12a797ec92f7fc77c0789b0e82c06
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-12-04 01:27:37 +0330

    added the mf for fuziffing gen

--- HW_fuzzy_Controller/GA_Fuzzy_Controller.py
+++ HW_fuzzy_Controller/GA_Fuzzy_Controller.py
@@ -18,7 +18,9 @@ class Fuzzy_Controller:
         self.mf_cm = {"low": mfs.trapmf_maker([0, float("inf")], 0, [0, 7e-1], 99e-2),
                       "high": mfs.trapmf_maker([0, float("inf")], 7e-1, [1, float("inf")], float("inf"))}
 
-        self.mf_gen = {}
+        self.mf_gen = {"start": mfs.trapmf_maker([0, 1], 0, [0, 4e-1], 6e-1),
+                       "middle": mfs.trimf_maker([0, 1], 0.4, 0.6, 0.8),
+                       "end": mfs.trapmf_maker([0, 1], 6e-1, [8e-1, 1], 1)}
 
     def fuzzifier(self, cur_gen, p_m, cur_bsf):
         membership_values = {"cm": None, "pm_prev": None, "gen": None}
@@ -38,34 +40,22 @@ class Fuzzy_Controller:
 
         membership_values["pm_prev"] = self.fuzzify_pm(p_m)
         membership_values['cm'] = self.fuzzify_cm(cm)
-        
+        membership_values['gen'] = self.fuzifify_generation(cur_gen)
 
     def fuzifify_generation(self, gen):
-
         """ this method gets the number of current generations
             and calculate the gen / max gen and fuzzify the result.
             it returns the result as a dictionary with three keys (start, middle, end)
             these keys are the labels of fuzzy sets """
 
-        x = gen / self.N
-        # for range 0 to 0.4 : start is 1 else is 0
-        if x < 0.4:
-            return {'start': 1, 'middle': 0, 'end': 0}
+        x = gen / self.mf_gen
+        start_mf = self.mf_gen["start"]
+        middle_mf = self.mf_gen["middle"]
+        end_mf = self.mf_gen["end"]
 
-        # for range 0.4 to 0.6 : start is -5x+3, middle is 5x-2, end is 0
-        elif x < 0.6:
-            return {'start': (-5 * x) + 3, 'middle': (5 * x) - 2, 'end': 0}
-
-        # for range 0.6 to 0.8 : start is 0, middle is -5x+4, end is 5x-3
-        elif x < 0.8:
-            return {'start': 0, 'middle': (-5 * x) + 4, 'end': (5 * x) - 3}
-
-        # for range 0.8 to 1 : end is 1 else is 0
-        else:
-            return {'start': 0, 'middle': 0, 'end': 1}
+        return {"start": start_mf(x), "middle": middle_mf(x), "end": end_mf(x)}
 
     def fuzzify_cm(self, cm):
-
         """ the set for cm in minimize and maximize is diffrent.
             to slve this problem and use uniq sets, we use diffrent formula 
             for cm.
