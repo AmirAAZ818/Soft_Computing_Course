5dc94734fbf6580a6f04db7c1981625d6152b21f
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2023-12-04 00:18:43 +0330

    Trapezius membership function and testing it

--- HW_fuzzy_Controller/Membership_functions.py
+++ HW_fuzzy_Controller/Membership_functions.py
@@ -12,17 +12,18 @@ def trimf_maker(domain, start, peak, end):
     """
 
     # we should assert this so that the method would be logically valid
-    assert start < peak < end, "Support is not valid"
+    assert start <= peak <= end, "Support is not valid"
 
     def trimf(x):
+        assert domain[0] <= x <= domain[1], "x is not in domain range!"
         if x <= start or x >= end:
             return 0
 
         if x == peak:
             return 1
 
-        before_peak_slope = 1 / (peak - start)
-        after_peak_slope = 1 / (end - peak)
+        before_peak_slope = 1 / (peak - start) if peak != start else 0
+        after_peak_slope = 1 / (peak - end) if peak != end else 0
 
         if x < peak:
             before_bias = -(start - domain[0]) * before_peak_slope
@@ -37,3 +38,39 @@ def trimf_maker(domain, start, peak, end):
             return res
 
     return trimf
+
+
+def trapmf_maker(domain, start, peak, end):
+    """
+    :param domain: 1d array of len 2
+    :param start: the starting point
+    :param peak:  1d array of len 2, showing the len of the peak value
+    :param end: the ending point
+    :return: a method that gives you membership capacity for a given x
+    """
+    assert domain[0] <= start and domain[1] >= end, "Domain or start or end is not valid"
+    assert start <= peak[0] <= peak[1] <= end, "parameters are not valid"
+
+    def trapmf(x):
+        if x <= start or x >= end:
+            return 0
+
+        if peak[0] <= x <= peak[1]:
+            return 1
+
+        before_peak_slope = 1 / (peak[0] - start) if peak[0] != start else 0
+        after_peak_slope = 1 / (peak[1] - end) if peak[1] != end else 0
+
+        if x < peak[0]:
+            before_bias = -(start - domain[0]) * before_peak_slope
+            res = (before_peak_slope * x) + before_bias
+
+            return res
+
+        if x > peak[1]:
+            after_bias = -(end - domain[0]) * after_peak_slope
+            res = (after_peak_slope * x) + after_bias
+
+            return res
+
+    return trapmf
--- HW_fuzzy_Controller/main.py
+++ HW_fuzzy_Controller/main.py
@@ -1,12 +1,23 @@
 from RGA_Class import RGA
 import theorem
-from Membership_functions import trimf_maker
-
+import matplotlib.pyplot as plt
+from Membership_functions import trimf_maker, trapmf_maker
+import numpy as np
 
 def main():
     # for test
-    mf1 = trimf_maker([0, 1e-2], 1e-3, 5e-3, 1e-2)
+    mf1 = trimf_maker([0, 10], 2, 5, 8)
+    mf2 = trapmf_maker([0,float("inf")], 0,[0,10], 10)
+    x = np.arange(0, 20, 1)
+    Y = [mf2(x[i]) for i in range(len(x))]
 
+
+    plt.plot(x, Y)
+    plt.title('trimf')
+    plt.xlabel('x')
+    plt.ylabel('Degree of Membership')
+    # plt.ylim([-0.05, 1.05])
+    plt.show()
 
 if __name__ == "__main__":
     main()
