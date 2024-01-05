9fc65b167fa328d47962928128029534700f36d8
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2024-01-05 20:57:18 +0330

    Fuzzy quera homework first question

--- Seminar/Implementation/main.py
+++ Seminar/Implementation/main.py
@@ -12,7 +12,7 @@ def main():
     # Griewank Function
     gsa_griewank = GSA(function=theorem.griewank, domain=[{'low': -600, 'high': 600}, {'low': -600, 'high': 600},
                                         {'low': -600, 'high': 600}, {'low': -600, 'high': 600}], function_dim=4, pop_size=300,
-                                        ittration=100,k=10, G=2.4, a=1, runs=1)
+                                        ittration=100, k=10, G=2.4, a=1, runs=1)
 
     gsa_griewank.run()
 
