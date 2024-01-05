bf8398a1ade477c4e491bec114ce1a34f482a382
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2024-01-05 22:24:08 +0330

    Update Draw_membership_Functions.py

--- HW_Fuzzy_Logic/Tamrin_1/Draw_membership_Functions.py
+++ HW_Fuzzy_Logic/Tamrin_1/Draw_membership_Functions.py
@@ -1,5 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import os
+
+"""
+    Run this file for testing and drawing the membership functions (Question 1, part 4)
+"""
+
 
 # 1.4
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# defining colors for having handsome strings
+blue = '\033[94m'
+reset = '\033[0m'
+red = '\033[91m'
+magenta = '\033[95m'
+cyan = '\033[96m'
+
+# start of the program
+
+if __name__ == "__main__":
+    clear_screen()
+    print("\n\n\n\n\n\n\n\n\n")
+    print(blue + "|________________________________________________________|" + reset)
+    print(blue + "| Welcome to our Homemade Membership Function Drawer :)  |")
+    print(blue + "|________________________________________________________|" + reset)
+
+    # cleaning the screen
+    time.sleep(2)
+    clear_screen()
+
+    print(cyan + "                                      \/\/\/\/\/ Please Choose a Membership function to draw \/\/\/\/\/" + reset + '\n')
+
+    print(red + "1. Triangle Membership Function")
+    print("2. Triangle Membership Function" + reset)
+
+    option = input(magenta + "Waiting for your Input: " + reset)
+
+    time.sleep(2)
+    clear_screen()
+
+    print("It was all a lie :)")
